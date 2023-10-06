646529bb099e6a6eb87beea0ceb55f94e0caac4d
prakharjn28 <123430982+prakharjn28@users.noreply.github.com>
2023-10-05 20:11:27 -0700

    Added new apis for plans and formulas

---
+++ database/plans.js
@@ -0,0 +1,44 @@
+const plans = [
+    {
+        id: 1,
+        sequence: [1, 2],
+    },
+    {
+        id: 2,
+        sequence: [2,3,4]
+    },
+    {
+        id: 3,
+        sequence: [1, 3]
+    }
+];
+
+function fetchPlansContainingFormula(formulaId) {
+   return plans.filter((plan) => plan.sequence.includes(formulaId))
+}
+
+function addFormulaToPlan(planIndex, formulaId) {
+    plans[planIndex].sequence.push(formulaId)
+    return plans[planIndex]
+}
+
+function addNewPlan(sequence) {
+    const newPlan = {
+        id: plans.length +1,
+        sequence
+    }
+    plans.push(newPlan);
+    return newPlan
+}
+
+function fetchPlan(planId) {
+    return plans.findIndex((p) => p.id === planId)
+}
+
+module.exports = {
+    plans,
+    fetchPlansContainingFormula,
+    addNewPlan,
+    fetchPlan,
+    addFormulaToPlan
+}
